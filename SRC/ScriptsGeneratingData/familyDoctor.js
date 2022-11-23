8d0c0e0659760b3feaae7f7456ec6e93dd6076bc
Martin Todev <mtodev@uwo.ca>
2022-11-22 22:03:16 -0500

    Fixed name

--- SRC/ScriptsGeneratingData/familyDoctor.js
+++ SRC/ScriptsGeneratingData/familyDoctor.js
@@ -6,7 +6,7 @@ let names = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../DataUsed/rand
 
 let emails = ["gmail.com", "yahoo.com", "hotmail.com", "aol.com", "msn.com", "wanadoo.fr", "orange.fr", "live.com", "free.fr", "gmx.de", "hotmail.it", "rogers.com", "hotmail.co.uk"];
 
-function generateOtherDoctor() {
+function generateDoctor() {
     let fNameI = getRandomInt(0, names.length-1);
     let lNameI = getRandomInt(0, names.length-1);
 
@@ -37,4 +37,4 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(generateOtherDoctor());
+console.log(generateDoctor());
