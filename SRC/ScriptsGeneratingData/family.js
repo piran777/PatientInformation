f46ce66efee92b90e6866de32487efa337a9911d
piran777 <paminul@uwo.ca>
2022-11-22 21:36:35 -0500

    added the family data adding script

---
+++ SRC/ScriptsGeneratingData/family.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+let relationshipTopatient = ["dad", "mom","sister","brother", "friend","uncle","aunt","cousin"];
+function generatePatient(patientIDs,familyIDs) {
+    
+    return {
+        relationshipTopatient: relationshipTopatient[getRandomInt(0, relationshipTopatient.length-1)],
+        patientID: patientIDs,
+        familyID : familyIDs,
+        
+}
+}
+function getRandomInt(min, max) {
+    min = Math.floor(min);
+    max = Math.floor(max);
+
+    if(max < min) {
+        let temp = max;
+        max = min;
+        min = temp;
+    }
+    
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(generatePatient(2333,23334));
