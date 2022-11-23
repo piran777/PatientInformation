262a307b3c9fe6f27389dcfe10bdebe111a0b8cd
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2022-11-22 19:43:19 -0500

    update illness probability data so that it generates all combinations

--- SRC/ScriptsGeneratingData/illnessProbabilityData.js
+++ SRC/ScriptsGeneratingData/illnessProbabilityData.js
@@ -4,32 +4,23 @@ const path = require('path');
 let illnessSymptoms = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../DataUsed/illnessSymptoms.json')));
 
 function generateIllnessProbabilityData() {
-    let i = getRandomInt(0, illnessSymptoms.length-1);
-    let ri = getRandomInt(0, illnessSymptoms.length-1);
+    let out = [];
 
-    while(ri === i) {
-        ri = getRandomInt(0, illnessSymptoms.length-1);
+    for(let illnessIndex = 0; illnessIndex < illnessSymptoms.length; illnessIndex++) {
+        for(let i = 0; i < illnessSymptoms.length; i++) {
+            if(i === illnessIndex) {
+                continue;
+            }
+
+            out.push({
+                illness : illnessSymptoms[illnessIndex]["name"],
+                resultingIllness : illnessSymptoms[i]["name"],
+                probabilityNotGetting : Math.floor(Math.random() * 10000)/10000,
+                probabilityThreshold : Math.floor(Math.random() * 2500)/10000
+            });
+        }
     }
-
-    return {
-        illness : illnessSymptoms[i]["name"],
-        resultingIllness : illnessSymptoms[ri]["name"],
-        probabilityNotGetting : Math.floor(Math.random() * 10000)/10000,
-        probabilityThreshold : Math.floor(Math.random() * 2500)/10000
-    }
-}
-
-function getRandomInt(min, max) {
-    min = Math.floor(min);
-    max = Math.floor(max);
-
-    if(max < min) {
-        let temp = max;
-        max = min;
-        min = temp;
-    }
-    
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return out;
 }
 
 console.log(generateIllnessProbabilityData());
