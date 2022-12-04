5d4669530ef4acb49f19b9fefb9f7ae30c57c5f2
Martin Todev <mtodev@uwo.ca>
2022-12-03 22:10:30 -0500

    Added search functions on backend

--- webapp/backend/index.js
+++ webapp/backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {startDatabaseConnection} = require('./databaseConnection');
+const {startDatabaseConnection, query} = require('./databaseConnection');
 
 const app = express();
 const port = 3000;
@@ -9,6 +9,23 @@ app.use('/', express.static('../frontend/build'));
 router.use(express.json());
 
 app.use('/api', router);
+
+
+//Get patient by healthcard number
+router.get('/patient/healthcard/:number', async (req, res) => {
+    const patientHealthCard = req.params.number
+    
+    const patient = await query(`SELECT c.* FROM patientcontactview c LEFT JOIN patient p ON p.firstName = c.firstName WHERE p.healthCardNumber = "${patientHealthCard}"`);
+    res.send(patient.result);
+})
+
+//Get patient by name
+router.get('/patient/name/:name', async (req, res) => {
+  const patientName = req.params.name.split(' ')
+
+  const patient = await query(`SELECT * FROM patientcontactview WHERE firstName = "${patientName[0]}" AND lastName = "${patientName[1]}"`);
+  res.send(patient.result);
+})
 
 
 startDatabaseConnection().then(async () => {
