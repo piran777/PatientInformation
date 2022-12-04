a8633cae93ad154c1dff156d0626003eab86f099
piran777 <paminul@uwo.ca>
2022-12-03 22:57:53 -0500

    added insertion to appointment

--- webapp/backend/index.js
+++ webapp/backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {startDatabaseConnection} = require('./databaseConnection');
+const{query} = require('./databaseConnection');
 
 const app = express();
 const port = 3000;
@@ -7,6 +8,42 @@ const port = 3000;
 const router = express.Router();
 app.use('/', express.static('../frontend/build'));
 router.use(express.json());
+
+let appointments ;
+
+
+
+router.post('/appointment', async (req,res)=>{
+  let identical = false;
+  
+  let sqlAppointment = await query(` INSERT INTO appointment(
+    startDateTime,
+    endDateTime,
+    notes,
+    reasonforAppointment,
+    FamilyDoctorMINC,patientHealthCardNumber
+    )
+    VALUES(
+    '${req.body.startDateTime}',
+    '${req.body.endDateTime}',
+    '${req.body.notes}',
+    '${req.body.reasonforAppointment}',
+    '${req.body.familyDoctorMINC}',
+    '${req.body.patientHealthCardNumber}'
+    )`);
+
+    let sqlViewAppointment = await query ( `SELECT  startDateTime,
+    endDateTime,
+    notes,
+    reasonforAppointment,
+    FamilyDoctorMINC,patientHealthCardNumber FROM appointment WHERE startDateTime = '${req.body.startDateTime}' AND  endDateTime ='${req.body.endDateTime}' AND NOTES =  '${req.body.notes}'
+    AND reasonforAppointment = '${req.body.reasonforAppointment}' AND familyDoctorMINC = '${req.body.familyDoctorMINC}' AND patientHealthCardNumber = '${req.body.patientHealthCardNumber}'` );
+     
+  console.log(sqlViewAppointment)
+  res.send(sqlViewAppointment);} //returns the new inserted appointments
+)
+
+
 
 app.use('/api', router);
 
