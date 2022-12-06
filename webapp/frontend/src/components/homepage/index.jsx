c9920486c9f18c18a439c117adef8cf6881fd470
piran777 <paminul@uwo.ca>
2022-12-05 21:44:42 -0500

    added appointment insert

--- webapp/frontend/src/components/homepage/index.jsx
+++ webapp/frontend/src/components/homepage/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import PatientOverview from '../PatientOverviewPage/PatientOverview'
+import Appointment from '../../appointment/Appointment'
 
 export default function Index() {
   return (<>
+  <></>
   <PatientOverview healthCardNumber={'0000000002ZZ'}/>
+  <Appointment />
   </>)
 }
