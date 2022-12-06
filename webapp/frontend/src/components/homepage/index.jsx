69b58431790c75e3ecd83d26b17ec93e2b40a3a5
Martin Todev <mtodev@uwo.ca>
2022-12-05 21:55:34 -0500

    Fixed index

--- webapp/frontend/src/components/homepage/index.jsx
+++ webapp/frontend/src/components/homepage/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PatientOverview from '../PatientOverviewPage/PatientOverview'
-import Appointment from '../../appointment/Appointment'
+import Appointment from '../appointment/Appointment'
 
 export default function Index() {
 
